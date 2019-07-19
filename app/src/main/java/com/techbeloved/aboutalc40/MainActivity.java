57e92868fc3c8c8e8762681812f0c165074724e9
odifek <kennedy998@gmail.com>
2019-07-19 12:33:06 +0100

    initial commit: project setup

---
+++ app/src/main/java/com/techbeloved/aboutalc40/MainActivity.java
@@ -0,0 +1,14 @@
+package com.techbeloved.aboutalc40;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
