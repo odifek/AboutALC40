3fdffb8c00ed07092caf227b42f49e878b336fd6
odifek <kennedy998@gmail.com>
2019-07-19 13:04:25 +0100

    create about and profile activities

--- app/src/main/java/com/techbeloved/aboutalc40/AboutActivity.java
+++ app/src/main/java/com/techbeloved/aboutalc40/AboutActivity.java
@@ -3,15 +3,12 @@ package com.techbeloved.aboutalc40;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class AboutActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
+        setContentView(R.layout.activity_about_alc);
     }
 }
--- app/src/main/java/com/techbeloved/aboutalc40/ProfileActivity.java
+++ app/src/main/java/com/techbeloved/aboutalc40/ProfileActivity.java
@@ -3,15 +3,12 @@ package com.techbeloved.aboutalc40;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
+        setContentView(R.layout.activity_my_profile);
     }
 }
